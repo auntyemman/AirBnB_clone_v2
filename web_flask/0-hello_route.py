538d9a8fc5c93ce84ea68b890e5a0adb0b3f0ced
auntyemman <1mole.coco@gmail.com>
2022-10-15 13:44:35 -0700

    Initial commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application"""
-
+""" A script that starts a flask web application """
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-"""A funtion that returns a string to the homepage"""
-def hello():
+def hello_hbnb():
+    """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
-    """IP and port app will run on"""
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
