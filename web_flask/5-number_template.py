4ba1675a22660bf1bc45892f7af2a3b4432e99f7
Adedayo Adepoju <1mole.coco@gmail.com>
2022-10-17 05:16:45 +0100

    initial commit

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" A script that starts a Flask Web Application """
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints a Message when /hbnb is called """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_variable(text):
+    """Prints C followed by some variables"""
+    new_text = text.replace('_', ' ')
+    return 'C ' + new_text
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is_cool'):
+    """ Prints a Message when /python is called """
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_variable(n):
+    """Prints an integer number when /number is called"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ display a HTML page only if n is an integer """
+    return render_template('5-number.html', value=n)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
